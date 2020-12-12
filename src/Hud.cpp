28659d94ca20d393c2ba3dfcc829cdc97afd4dd1
Vodable <vodableemma@gmail.com>
2020-12-12 19:15:49 +0100

    correction & update Gui : correct  texture size and position in OpenGL view

--- src/Hud.cpp
+++ src/Hud.cpp
@@ -2,8 +2,11 @@
 
 Hud::Hud()
 {
-	Panel newpanel("res/img/uiplaceholder.png", "ui", 0.5,0.5,0.4);
-	_panels.push_back(newpanel);
+	Panel panel1("res/img/uiplaceholder.png", "ui", 0.5, 0.5, 0.4);
+	_panels.push_back(panel1);
+
+	Panel panel2("res/img/ui_inventory.png", "ui_inventory", 0.9, 0.0, 1.0);
+	_panels.push_back(panel2);
 }
 
 Hud::~Hud()
@@ -12,8 +15,10 @@ Hud::~Hud()
 
 void Hud::Draw() const
 {
+	glDisable(GL_DEPTH_TEST);
 	for (Panel panel : _panels) {
 		panel.Draw();
 	}
+	glEnable(GL_DEPTH_TEST);
 }
 
--- src/Panel.cpp
+++ src/Panel.cpp
@@ -1,20 +1,31 @@
 
 #include "Panel.hpp"
+#include "Material.hpp"
+
 
 Panel::Panel(const std::string texture, std::string name, float x, float y, float scale):
 	_modelMatrix(glm::mat4(1.0f)), _name(name)
 {
+	//
+	// [TODO] :: Global window size
+	//
+
+	float imgWidth = ResourceManager::Get().LoadTexture(texture, DIFFUSE).Width();
+	float width = imgWidth / 1080.0;
+	float imgHeight = ResourceManager::Get().LoadTexture(texture, DIFFUSE).Height();
+	float height = imgHeight / 720.0;
 
 	std::vector<ShapeVertex> vertices = {
-				// position					// normal				// texcoord
-		ShapeVertex(glm::vec3(-1.0, 1.0, 0.0), glm::vec3(0.0, 0.0, 0.0), glm::vec2(0.0, 0.0)),
-		ShapeVertex(glm::vec3(1.0, 1.0, 0.0), glm::vec3(0.0, 0.0, 0.0), glm::vec2(0.0, 1.0)),
-		ShapeVertex(glm::vec3(1.0,-1.0, 0.0), glm::vec3(0.0, 0.0, 0.0), glm::vec2(1.0, 1.0)),
-		ShapeVertex(glm::vec3(-1.0,-1.0, 0.0), glm::vec3(0.0, 0.0, 0.0), glm::vec2(1.0, 0.0))
+				// position								// normal				// texcoord
+		ShapeVertex(glm::vec3(-width, height, 0.0), glm::vec3(0.0, 0.0, 0.0), glm::vec2(0.0, 0.0)),
+		ShapeVertex(glm::vec3( width, height, 0.0), glm::vec3(0.0, 0.0, 0.0), glm::vec2(1.0, 0.0)),
+		ShapeVertex(glm::vec3( width,-height, 0.0), glm::vec3(0.0, 0.0, 0.0), glm::vec2(1.0, 1.0)),
+		ShapeVertex(glm::vec3(-width,-height, 0.0), glm::vec3(0.0, 0.0, 0.0), glm::vec2(0.0, 1.0))
 	};
 
 
-	const auto material = ResourceManager::Get().CacheBasicMaterial(_name, texture);
+	auto material = ResourceManager::Get().CacheBasicMaterial(_name, texture);
+	
 
 	std::vector<unsigned int> indices = {
 		0,1,3,
