4cb2ab364f2ef441c32d475b572b0df5b14adb7f
evodable <vodableemma@gmail.com>
2020-12-13 19:45:41 +0100

    UI graphism implemented

--- src/Hud.cpp
+++ src/Hud.cpp
@@ -2,14 +2,24 @@
 
 Hud::Hud()
 {
-	Panel panel1("res/img/uiplaceholder.png", "ui", 0.5, 0.5, 0.4, 1280, 1);
+	Panel panel1("res/img/ui_use.png", "ui_use", -0.3, -0.9, 1.0, 132, 1);
 	_panels.push_back(panel1);
 
-	Panel panel2("res/img/ui_inventory.png", "ui_inventory", 0.9, 0.3, 0.6, 93, 2);
+	Panel panel2("res/img/ui_observe.png", "ui_observe", 0.0, -0.9, 1.0, 147, 1);
 	_panels.push_back(panel2);
 
-	Panel panel3("res/img/ui_crystals.png", "ui_crystal", 0.8, -0.7, 1.5, 101, 5);
+	Panel panel3("res/img/ui_crystal.png", "ui_crystal", 0.8, -0.7, 1.0, 163, 5);
 	_panels.push_back(panel3);
+
+	Panel panel4("res/img/ui_health.png", "ui_health", 0.8, -0.7, 1.0, 163, 8);
+	_panels.push_back(panel4);
+
+	Panel panel5("res/img/ui_key.png", "ui_key", 0.9, 0.3, 1.0, 80, 3);
+	_panels.push_back(panel5);
+
+	Panel panel6("res/img/ui_map.png", "ui_map", 0.9, 0.0, 1.0, 80, 3);
+	_panels.push_back(panel6);
+
 }
 
 Hud::~Hud()
